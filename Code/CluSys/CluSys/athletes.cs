b66a0e0ff41ef223e8f1c06b4e64b88e4b8adb06
Ricardo Jesus <rj.bcjesus@gmail.com>
2017-04-27 12:51:26 +0100

    Some improvements over the overall layout

---
+++ Code/CluSys/CluSys/athletes.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CluSys
+{
+    class Athletes
+    {
+    }
+
+    /// TEMPLATES/EXAMPLES
+    public class Customer
+    {
+        public String FirstName { get; set; }
+        public String LastName { get; set; }
+        public String Address { get; set; }
+
+        public Customer(String firstName, String lastName, String address)
+        {
+            this.FirstName = firstName;
+            this.LastName = lastName;
+            this.Address = address;
+        }
+
+    }
+
+    public class Customers : ObservableCollection<Customer>
+    {
+        public Customers()
+        {
+            Add(new Customer("Michael", "Anderberg",
+                    "12 North Third Street, Apartment 45"));
+            Add(new Customer("Chris", "Ashton",
+                    "34 West Fifth Street, Apartment 67"));
+            Add(new Customer("Cassie", "Hicks",
+                    "56 East Seventh Street, Apartment 89"));
+            Add(new Customer("Guido", "Pica",
+                    "78 South Ninth Street, Apartment 10"));
+        }
+    }
+}
