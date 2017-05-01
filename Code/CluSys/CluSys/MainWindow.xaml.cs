a3d910c6208083d3902588b72ab91a979171c788
Ricardo Jesus <rj.bcjesus@gmail.com>
2017-05-01 18:51:44 +0100

    Started to use Material framework - looks much better

--- Code/CluSys/CluSys/MainWindow.xaml.cs
+++ Code/CluSys/CluSys/MainWindow.xaml.cs
@@ -24,5 +24,10 @@ namespace CluSys
         {
             InitializeComponent();
         }
+
+        private void Search_OnKeyDown(object sender, KeyEventArgs e)
+        {
+
+        }
     }
 }
