ea1478b925f45be251e696b166a6ce8326c0f6e1
Tiago Ramalho <t.ramalho22@ua.pt>
2017-05-04 12:12:29 +0100

    insert's na base de dados e C# files para BD

---
+++ BD/C#_class/MedicalHistoryExams.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+public class MedicalHistoryExams
+{
+    private int MHId;
+    private String Exam;
+
+    public int MHId
+    {
+        get { return MHId; }
+        set { MHId = value; }
+    }
+    public string Exam
+    {
+        get { return Exam;}
+        set { Exam = value; }
+    }
+
+    public MedicalHistoryExams()
+	{
+
+	}
+
+    private void submitMedicalHistoryExams(MedicalHistoryExams ME)
+    {
+        if (!verifySGBDConnection())
+            return;
+        SqlCommand cmd = new SqlCommand();
+        cmd.ComandText = "INSERT INTO MedicalHistoryExams (MHId, Exam)" + "VALUES (@MHId, @Exam)";
+        cmd.Parameters.AddWithValue("@MHId", ME.MHId);
+        cmd.Parameters.AddWithValue("@Exam", ME.Exam);
+        cmd.Connection = cn;
+
+        try
+        {
+            cmd.ExecuteNonQuery();
+        }
+        catch (Exception ex)
+        {
+            throw new Exception("Failed to Insert Medical History Exam in database. \n ERROR MESSAGE: \n" + ex.Message);
+        }
+        finally
+        {
+            cn.Close();
+        }
+
+    }
+
+    private void selectMedicaHistoryExam()
+    {
+        if (!verifySGBDConnection())
+            return;
+
+        SqlCommand cmd = new SqlCommand("SELECT * FROM MedicalHistoryExams", cn);
+        SqlDataReader reader = cmd.ExecuteReader();
+        listBox1.Items.Clear();
+        while (reader.Read())
+        {
+            MedicalHistoryExams ME = new MedicalHistoryExams();
+            ME.MHId = reader["MHId"].ToString();
+            ME.Exam = reader["Exam"].ToString();
+            listBox1.Items.Add(ME);
+
+        }
+        cn.Close();
+
+    }
+
+
+}
