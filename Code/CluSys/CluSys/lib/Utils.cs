221a523404595bb1f17d6f175d7769728c14ce83
Ricardo Jesus <rj.bcjesus@gmail.com>
2017-05-13 19:12:22 +0100

    Basic modal refactor

--- Code/CluSys/CluSys/lib/Utils.cs
+++ Code/CluSys/CluSys/lib/Utils.cs
@@ -55,4 +55,17 @@ namespace CluSys.lib
 
         public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture) { throw new NotSupportedException(); }
     }
+
+    public sealed class InvertBoolConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return !(bool?) value;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return !(bool?) value;
+        }
+    }
 }
