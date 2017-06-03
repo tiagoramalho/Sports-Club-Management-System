e1e596e9cbd81736040a725c25d31416c4fe4626
Tiago Ramalho <t.ramalho22@ua.pt>
2017-06-03 12:28:38 +0100

    BD inserts

--- Code/CluSys/CluSys/Management.xaml.cs
+++ Code/CluSys/CluSys/Management.xaml.cs
@@ -1,5 +1,9 @@
-﻿using System;
+﻿using CluSys.lib;
+using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Data;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -19,35 +23,58 @@ namespace CluSys
     /// </summary>
     public partial class Management 
     {
+        ObservableCollection<Modality> ListOfModalities => Modalities.GetModalities();
+
         public Management()
         {
             InitializeComponent();
         }
         public void InsertModality(object sender, RoutedEventArgs e){
-            using (var cmd = new SqlCommand("CluSys.P_AddModality", cn) { CommandType = CommandType.StoredProcedure })
+            using (var cn = lib.ClusysUtils.GetConnection())
             {
-                cmd.Parameters.Add(new SqlParameter("@Name", SqlDbType.NVarChar));
-                cmd.Parameters.Add(new SqlParameter("@Year", SqlDbType.SMALLINT));
-                foreach (var prob in Problems)
+                cn.Open();
+                using (var cmd = new SqlCommand("CluSys.P_AddModality", cn) { CommandType = CommandType.StoredProcedure })
                 {
+                    cmd.Parameters.Add(new SqlParameter("@Name", SqlDbType.NVarChar));
+                    cmd.Parameters.Add(new SqlParameter("@Year", SqlDbType.SmallInt));
                     cmd.Parameters["@Name"].Value = this.ModalityName.Text;
                     cmd.Parameters["@Year"].Value = this.RecognitionYear.Text;
                     cmd.ExecuteNonQuery();
                 }
             }
+            Console.WriteLine(this.ModalityName.Text);
         }
         public void InsertClass(object sender, RoutedEventArgs e){
-            using (var cmd = new SqlCommand("CluSys.P_AddClass", cn) { CommandType = CommandType.StoredProcedure })
+            using (var cn = lib.ClusysUtils.GetConnection())
             {
-                cmd.Parameters.Add(new SqlParameter("@ModalityId", SqlDbType.NVarChar));
-                cmd.Parameters.Add(new SqlParameter("@Name", SqlDbType.NVarChar));
-                 cmd.Parameters.Add(new SqlParameter("@InitialAge", SqlDbType.TINYINT));
-                cmd.Parameters.Add(new SqlParameter("@FinalAge", SqlDbType.TINYINT));
-                foreach (var prob in Problems)
+                cn.Open();
+                using (var cmd = new SqlCommand("CluSys.P_AddClass", cn) { CommandType = CommandType.StoredProcedure })
                 {
+                    cmd.Parameters.Add(new SqlParameter("@ModalityId", SqlDbType.NVarChar));
+                    cmd.Parameters.Add(new SqlParameter("@Name", SqlDbType.NVarChar));
+                    cmd.Parameters.Add(new SqlParameter("@InitialAge", SqlDbType.TinyInt));
+                    cmd.Parameters.Add(new SqlParameter("@FinalAge", SqlDbType.TinyInt));
                     cmd.Parameters["@ModalityId"].Value = this.ModalityID.Text;
                     cmd.Parameters["@Name"].Value = this.ClassName.Text;
-                    cmd.Parameters["@InitialAge"].Value =  this.InitialAge.Text;
+                    cmd.Parameters["@InitialAge"].Value = this.InitialAge.Text;
+                    cmd.Parameters["@FinalAge"].Value = this.FinalAge.Text;
+                    cmd.ExecuteNonQuery();
+                }
+            }
+        }
+        public void InsertAthlete(object sender, RoutedEventArgs e){
+            using (var cn = lib.ClusysUtils.GetConnection())
+            {
+                cn.Open();
+                using (var cmd = new SqlCommand("CluSys.P_AddClass", cn) { CommandType = CommandType.StoredProcedure })
+                {
+                    cmd.Parameters.Add(new SqlParameter("@ModalityId", SqlDbType.NVarChar));
+                    cmd.Parameters.Add(new SqlParameter("@Name", SqlDbType.NVarChar));
+                    cmd.Parameters.Add(new SqlParameter("@InitialAge", SqlDbType.TinyInt));
+                    cmd.Parameters.Add(new SqlParameter("@FinalAge", SqlDbType.TinyInt));
+                    cmd.Parameters["@ModalityId"].Value = this.ModalityID.Text;
+                    cmd.Parameters["@Name"].Value = this.ClassName.Text;
+                    cmd.Parameters["@InitialAge"].Value = this.InitialAge.Text;
                     cmd.Parameters["@FinalAge"].Value = this.FinalAge.Text;
                     cmd.ExecuteNonQuery();
                 }
